871bd4f3c18a23c3241158f84dc745ed27a1e2a2
Julien Bernard <julien.bernard@univ-fcomte.fr>
2025-01-20 19:33:59 +0100

    add map collisions

--- code/bits/GameState.cc
+++ code/bits/GameState.cc
@@ -1,5 +1,9 @@
 #include "GameState.h"
+#include "MapSettings.h"
 
+#include <gf/Log.h>
+#include <gf/Geometry.h>
+#include <gf/Polyline.h>
 #include <gf/Serialization.h>
 #include <gf/SerializationOps.h>
 #include <gf/Streams.h>
@@ -7,13 +11,68 @@
 namespace be {
 
   namespace {
-    constexpr cpFloat HeroMass = 10.0f;
-    constexpr cpFloat HeroRadius = 32.0f;
+    constexpr cpFloat HeroMass = 10.0;
+    constexpr cpFloat HeroRadius = 32.0;
 
-    constexpr cpFloat ProducerRadius = 32.0f;
+    constexpr cpFloat ProducerRadius = 32.0;
+
+    constexpr cpFloat MapRadius = 0.1;
 
     gf::Vector2f toVec(cpVect raw) {
       return gf::vec(raw.x, raw.y);
+    }
+
+    std::vector<gf::Polyline> computeMapCollisions(const MapState& map)
+    {
+      std::vector<gf::SegmentI> segments;
+
+      for (auto position : map.cells.getPositionRange()) {
+        if (map.cells(position).type != CellType::Ground) {
+          continue;
+        }
+
+        const gf::Vector2i base = position * TileSize;
+
+        const gf::Vector2i up = position + gf::diry(-1);
+
+        if (map.cells.isValid(up) && map.cells(up).type != CellType::Ground) {
+          gf::SegmentI segment = {};
+          segment.p0 = base;
+          segment.p1 = base + gf::dirx(TileSize.x);
+          segments.push_back(segment);
+        }
+
+        const gf::Vector2i left = position + gf::dirx(-1);
+
+        if (map.cells.isValid(left) && map.cells(left).type != CellType::Ground) {
+          gf::SegmentI segment = {};
+          segment.p0 = base;
+          segment.p1 = base + gf::diry(TileSize.y);
+          segments.push_back(segment);
+        }
+
+        const gf::Vector2i down = position + gf::diry(+1);
+
+        if (map.cells.isValid(down) && map.cells(down).type != CellType::Ground) {
+          gf::SegmentI segment = {};
+          segment.p0 = base + gf::diry(TileSize.y);
+          segment.p1 = base + gf::diry(TileSize.y) + gf::dirx(TileSize.x);
+          segments.push_back(segment);
+        }
+
+        const gf::Vector2i right = position + gf::dirx(+1);
+
+        if (map.cells.isValid(right) && map.cells(right).type != CellType::Ground) {
+          gf::SegmentI segment = {};
+          segment.p0 = base + gf::dirx(TileSize.x);
+          segment.p1 = base + gf::dirx(TileSize.x) + gf::diry(TileSize.y);
+          segments.push_back(segment);
+        }
+      }
+
+      gf::Log::debug("Segments: %zu\n", segments.size());
+
+      return gf::buildLines(segments);
     }
 
   }
@@ -21,6 +80,42 @@ namespace be {
   void GameState::initializePhysics()
   {
     cpSpace* space = physics.getSpace();
+
+    /*
+     * Map
+     */
+
+    auto mapBody = cpBodyNewStatic();
+    cpSpaceAddBody(space, mapBody);
+    auto mapCollisions = computeMapCollisions(map);
+
+    gf::Log::debug("Collisions: %zu\n", mapCollisions.size());
+
+    for (auto& polyline : mapCollisions) {
+      const std::size_t last = polyline.getPointCount() - 1;
+
+      for (std::size_t i = 0; i < last; ++i) {
+        const gf::Vector2i curr = polyline.getPoint(i);
+        const gf::Vector2i next = polyline.getNextPoint(i);
+        const gf::Vector2i currNeighbor = polyline.hasPrevPoint(i) ? static_cast<const gf::Vector2i>(polyline.getPrevPoint(i)) : curr;
+        const gf::Vector2i nextNeighbor = polyline.hasNextPoint(i + 1) ? static_cast<const gf::Vector2i>(polyline.getNextPoint(i + 1)) : next;
+
+        auto shape = cpSegmentShapeNew(mapBody, cpv(curr.x, curr.y), cpv(next.x, next.y), MapRadius);
+        cpSegmentShapeSetNeighbors(shape, cpv(currNeighbor.x, currNeighbor.y), cpv(nextNeighbor.x, nextNeighbor.y));
+        cpSpaceAddShape(space, shape);
+      }
+
+      if (polyline.isLoop()) {
+        const gf::Vector2i curr = polyline.getPoint(last);
+        const gf::Vector2i next = polyline.getPoint(0);
+        const gf::Vector2i currNeighbor = polyline.getPrevPoint(last);
+        const gf::Vector2i nextNeighbor = polyline.getNextPoint(0);
+
+        auto shape = cpSegmentShapeNew(mapBody, cpv(curr.x, curr.y), cpv(next.x, next.y), MapRadius);
+        cpSegmentShapeSetNeighbors(shape, cpv(currNeighbor.x, currNeighbor.y), cpv(nextNeighbor.x, nextNeighbor.y));
+        cpSpaceAddShape(space, shape);
+      }
+    }
 
     /*
      * Hero
@@ -59,6 +154,7 @@ namespace be {
       auto producerShape = cpCircleShapeNew(producersBody, ProducerRadius, cpv(producer.location.x, producer.location.y));
       cpSpaceAddShape(space, producerShape);
     }
+
 
   }
 
