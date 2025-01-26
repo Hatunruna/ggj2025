3da9ae4c1307809f35cfcdb2bb30be7dff5683fd
ahugeat <hugeat.arthur@gmail.com>
2025-01-26 16:18:01 +0100

    Display player on minimap

--- code/bits/MiniMapEntity.cc
+++ code/bits/MiniMapEntity.cc
@@ -1,10 +1,12 @@
 #include "MiniMapEntity.h"
 
 #include <gf/Sprite.h>
+#include <gf/Vector.h>
 
 #include "GameHub.h"
 #include "MapSettings.h"
 #include "MapState.h"
+#include "Support.h"
 
 namespace be {
 
@@ -47,6 +49,12 @@ namespace be {
       if (m_state.miniMap.visited(position) == MiniMapStatus::Visited) {
         image.setPixel(position, gf::Color::toRgba32(gf::Color::Transparent));
       }
+    }
+
+    const auto heroPosition = gf::Vector2i(m_state.hero.location / TileSize);
+    const auto points = generateCircle(heroPosition, 5);
+    for (const auto& point: points) {
+      image.setPixel(point, { 0xFF, 0xFF, 0x00, 0xFF});
     }
 
     m_fogTexture.update(image);
